d01748506810672c7094101efe371c0d99dd0f7d
Warren Harmon <harmonw@cliu.org>
2025-11-26 11:15:19 -0500

    on error, pass user_name back from process.php to upload.php

--- process.php
+++ process.php
@@ -39,7 +39,7 @@ function verify_dir($path) {
 	if (! is_dir($path)) {
 		?>
 	<h1>Error: directory '<?=$data_month?>' does not exist!</h1>
-	Please <a href="upload.php?data_month=<?=$data_month?>">upload the files for the report</a>
+	Please <a href="upload.php?data_month=<?=$data_month?>&user_name=<?=user_name?>">upload the files for the report</a>
 		<?php
 		exit();
 	}
